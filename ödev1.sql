66028cff7a22437fa2dfdb816b1f87d4f18f9db1
baharsenturk <senturk.2005.108@gmail.com>
2025-03-05 22:44:29 +0300

    Add files via upload

---
+++ ödev1.sql
@@ -0,0 +1,10 @@
+select employees.firstname , employees.lastname , employees.age , employees.departmentid , employees.salary , employees.joindate from employees
+
+select distinct departmentid, departmentname from departments
+
+select distinct departmentid from departments
+
+select employeeid , firstname , lastname , age , departmentid , joindate , salary from employees order by salary desc
+
+select firstname ||':'|| lastname from employees
+
